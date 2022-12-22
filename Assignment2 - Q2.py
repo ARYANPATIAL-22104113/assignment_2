5a430f7d23d2d3664ea6d2cf9e8efc8c556845a3
ARYANPATIAL-22104113 <121176914+ARYANPATIAL-22104113@users.noreply.github.com>
2022-12-22 08:21:29 +0000

    Add files via upload

---
+++ Assignment2 - Q2.py
@@ -0,0 +1,5 @@
+name = "ABC"
+SID = "2210XXXX"
+department="XYZ"
+CGPA = 9.9
+print("Hey,",name,"here! my SID is ",SID,"I am from", department,"department and my CGPA is",CGPA)
---
+++ Assignment2 -Q3.py
@@ -0,0 +1,7 @@
+a = 56
+b = 10
+print("a&b =",a&b)
+print("a/b=",a/b)
+print("a^b=",a^b)
+print("Left shifting both a andb with 2 bits=",a<<2,"=",b<<2)
+print("Right shifting a with 2 bits and b with 4 bits =",a>>2,"=",b<<4)
